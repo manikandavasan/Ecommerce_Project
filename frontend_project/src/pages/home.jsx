11c62e8c2a176b184ae876b3dc1ffbf9d1ccc6d9
Mani <m45589207@gmail.com>
2026-04-29 11:11:42 +0530

    home image url updated

--- frontend_project/src/pages/home.jsx
+++ frontend_project/src/pages/home.jsx
@@ -135,7 +135,7 @@ export default function Home() {
                   <div
                     className="carousel-bg carousel-image"
                     style={{
-                      backgroundImage: `url("https://ecommerce-project-hm4w.onrender.com${product.image}")`,
+                      backgroundImage: `url("https://ecommerce-project-hm4w.onrender.com/${product.image}")`,
                       backgroundSize: "80% 100%",
                       backgroundPosition: "center",
                       height: "650px"
